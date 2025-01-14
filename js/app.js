1edbeb60f9c78494c19141fe3b1b1fa50a5d832f
Bri A <alexiellis22@gmail.com>
2025-01-14 14:14:23 -0500

    Output guess to current-guess span/Update README.md

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const guessMessageElement = document.getElementById("guess-message");
+const currentGuessElement = document.getElementById("current-guess");
+const computerGuessElement = document.getElementById("computer-guess");
+const guessHistoryElement = document.getElementById("guess-history");
+const guessInput = document.getElementById("guess-input");
+const submitBtn = document.getElementById("submit-btn");
+const restartBtn = document.getElementById("restart-btn");
+
+
+
+
+function userGuess() {
+   
+  currentGuessElement.innerText = guessInput.value;
+}
+
+submitBtn.addEventListener("click", function () {
+  userGuess();
+});
