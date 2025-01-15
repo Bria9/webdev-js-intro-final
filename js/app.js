bb00e455c990908a3795b640c4689a401e8ce19f
Bri A <alexiellis22@gmail.com>
2025-01-15 15:27:44 -0500

    Create functionality for computer guess, add validation message to user guess/Update README

--- js/app.js
+++ js/app.js
@@ -12,10 +12,36 @@ const restartBtn = document.getElementById("restart-btn");
 
 
 function userGuess() {
-   
-  currentGuessElement.innerText = guessInput.value;
+  let userValue =  Number(guessInput.value);
+
+  if(userValue !== '' && (isNaN(userValue) || userValue < 1 || userValue > 10)){
+    document.getElementById("error").style.display = "block";
+  }
+  else{
+    document.getElementById("error").style.display = "none";
+  }
+  currentGuessElement.innerText = userValue;
+}
+
+
+function computerGuess() {
+  let randomNum = Math.floor(Math.random() * 10) + 1; 
+
+    computerGuessElement.innerText = randomNum;
+
+}
+
+
+
+function render() {
+    userGuess();
+    computerGuess();
 }
 
 submitBtn.addEventListener("click", function () {
-  userGuess();
+
+
+
+  
+  render();
 });
