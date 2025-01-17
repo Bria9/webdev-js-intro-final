718a0a94b97840fe0ab9cdf47c1e1b56b356b044
Bri A <alexiellis22@gmail.com>
2025-01-16 21:37:18 -0500

    Add functionality for submit/restart buttons, output guess-message/Update README

--- js/app.js
+++ js/app.js
@@ -4,44 +4,92 @@ const guessMessageElement = document.getElementById("guess-message");
 const currentGuessElement = document.getElementById("current-guess");
 const computerGuessElement = document.getElementById("computer-guess");
 const guessHistoryElement = document.getElementById("guess-history");
-const guessInput = document.getElementById("guess-input");
 const submitBtn = document.getElementById("submit-btn");
 const restartBtn = document.getElementById("restart-btn");
+
+const guessInput = document.getElementById("guess-input");
+let randomNum;
+let attempt = 0;
 
 
 
 
 function userGuess() {
-  let userValue =  Number(guessInput.value);
-
-  if(userValue !== '' && (isNaN(userValue) || userValue < 1 || userValue > 10)){
-    document.getElementById("error").style.display = "block";
-  }
-  else{
-    document.getElementById("error").style.display = "none";
-  }
-  currentGuessElement.innerText = userValue;
+ let userguess = Number(guessInput.value)
+ currentGuessElement.innerText = userguess;
+return userguess;
+ 
 }
+
 
 
 function computerGuess() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
+   randomNum = Math.floor(Math.random() * 10) + 1; 
 
-    computerGuessElement.innerText = randomNum;
+  //  computerGuessElement.innerText = randomNum;
+   return randomNum;
 
 }
+computerGuess();
+
+
+
+function guessMessage(){
+  let userValue = userGuess();
+  
+
+      if(userValue === randomNum){
+      guessMessageElement.innerText = "You win! 🎉";
+      computerGuessElement.innerText = randomNum;
+      submitBtn.disabled = true;
+      restartBtn.disabled = false;
+
+      }
+      else if (userValue < randomNum){
+        guessMessageElement.innerText = "Number is too low";
+      }
+      else if (userValue > randomNum){
+        guessMessageElement.innerText = "Number is too high";
+      }
+  
+}
+
+
 
 
 
 function render() {
-    userGuess();
-    computerGuess();
+  attempt++;
+  
+  if(attempt > 2){
+    guessMessageElement.innerText = "Sorry, you loose...";
+    computerGuessElement.innerText = randomNum;
+    submitBtn.disabled = true;
+    restartBtn.disabled = false;}
+    else{ guessMessage(); }
+
+
+  console.log(userGuess());
+   console.log(randomNum);
+   console.log(`this is attempt: ${attempt}`);
+    
+   
 }
 
 submitBtn.addEventListener("click", function () {
-
-
-
-  
+ 
   render();
+
 });
+restartBtn.addEventListener("click", function () {
+ 
+  submitBtn.disabled = false;
+  restartBtn.disabled = true;
+  guessMessageElement.innerText = "";
+  guessInput.value = "";
+  randomNum = 0;
+  attempt = 0;
+  computerGuess();
+
+});
+
